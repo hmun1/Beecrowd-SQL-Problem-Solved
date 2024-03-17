aa5198b66d9535ab39cf88ff21c831b8662971d2
hmun1 <152993524+hmun1@users.noreply.github.com>
2024-03-17 22:09:45 +0600

    Beecrowd SQL problem 2604 Solved

---
+++ beecrowd_2604.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE beecrowd_2604;
+
+USE beecrowd_2604;
+
+CREATE TABLE products (
+id INT PRIMARY KEY,
+name VARCHAR(50),
+amount INT,
+price FLOAT
+);
+
+INSERT INTO products(id, name, amount, price)
+VALUES
+(1, 'Two-door wardrobe', 100, 80),
+(2, 'Dining table', 1000, 560),
+(3, 'Towel holder', 10000, 5.50),
+(4, 'Computer desk', 350, 100),
+(5, 'Chair', 3000, 210.64),
+(6, 'Single bed',  750, 99);
+
+SELECT id, name 
+FROM products
+WHERE price < 10 OR price > 100;
+
+
